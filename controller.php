815f6f20606be3d072fd7caa5b39e3627c63a256
Raza ul Mustafa <134964347+Hello4422@users.noreply.github.com>
2023-07-24 13:54:55 +0500

    Update controller.php
    Refractoring included, exception handling included

--- controller.php
+++ controller.php
@@ -1,89 +1,171 @@
-<?php require 'db.php'; ?>
+// <?php require 'db.php'; ?>
 
-<?php 
+// <?php 
 
 
-class Post{
+// class Post{
 
 	
 
-//CREATE POST
-	public function insertPost($title,$content){
+// //CREATE POST
+// 	public function insertPost($title,$content){
 
-		global $pdo;
+// 		global $pdo;
 
-		$stmt = $pdo->prepare('INSERT INTO user_post (title,content) VALUES (?,?)');
+// 		$stmt = $pdo->prepare('INSERT INTO user_post (title,content) VALUES (?,?)');
 
-		$stmt->execute([
+// 		$stmt->execute([
 
-			$title,
-			$content 
+// 			$title,
+// 			$content 
 		
 
-		]);
+// 		]);
 
-		return $pdo->lastInsertId();
-	}
-
-
-//READ POST
+// 		return $pdo->lastInsertId();
+// 	}
 
 
-	public function readAllPost($fetchType = PDO::FETCH_OBJ){
+// //READ POST
 
-		global $pdo;
 
-		$stmt = $pdo->prepare('SELECT * FROM user_post ORDER BY id DESC');
-		$stmt->execute();
+// 	public function readAllPost($fetchType = PDO::FETCH_OBJ){
 
-		return $stmt->fetchAll($fetchType);
+// 		global $pdo;
 
-	}
+// 		$stmt = $pdo->prepare('SELECT * FROM user_post ORDER BY id DESC');
+// 		$stmt->execute();
 
-	public function readPostById($id, $fetchType = PDO::FETCH_OBJ){
+// 		return $stmt->fetchAll($fetchType);
 
-		global $pdo;
+// 	}
 
-		$stmt = $pdo->prepare('SELECT * FROM user_post WHERE id=?');
+// 	public function readPostById($id, $fetchType = PDO::FETCH_OBJ){
+
+// 		global $pdo;
+
+// 		$stmt = $pdo->prepare('SELECT * FROM user_post WHERE id=?');
 		
-		$stmt->execute([$id]);
+// 		$stmt->execute([$id]);
 
-		return $stmt->fetch($fetchType);
+// 		return $stmt->fetch($fetchType);
 
-	}
-
-
-	//UPDATE POST
+// 	}
 
 
-	public function updatePost($id,$title,$content){
-
-global $pdo;
-
-$stmt = $pdo->prepare('UPDATE user_post SET title=?, content=? WHERE id=?');
-
-$stmt->execute([
-
-$title,
-$content,
-$id
-
-]);
+// 	//UPDATE POST
 
 
-	}
+// 	public function updatePost($id,$title,$content){
+
+// global $pdo;
+
+// $stmt = $pdo->prepare('UPDATE user_post SET title=?, content=? WHERE id=?');
+
+// $stmt->execute([
+
+// $title,
+// $content,
+// $id
+
+// ]);
 
 
+// 	}
 
 
 
-public function deletePost($id){
 
-	global $pdo;
 
-	$stmt = $pdo->prepare("DELETE FROM user_post WHERE id=?");
+// public function deletePost($id){
 
-	$stmt->execute([$id]);
+// 	global $pdo;
+
+// 	$stmt = $pdo->prepare("DELETE FROM user_post WHERE id=?");
+
+// 	$stmt->execute([$id]);
+// }
+// <div></div>
+// }
+
+
+
+
+
+<?php
+require 'db.php';
+
+class Post{
+    // ...
+
+    // CREATE POST
+    public function insertPost($title, $content){
+        global $pdo;
+
+        try {
+            $stmt = $pdo->prepare('INSERT INTO user_post (title, content) VALUES (?, ?)');
+            $stmt->execute([$title, $content]);
+            return $pdo->lastInsertId();
+        } catch (PDOException $e) {
+            // Handle the exception, you can log it or return an error message
+            return false;
+        }
+    }
+
+    // READ POST
+    public function readAllPost($fetchType = PDO::FETCH_OBJ){
+        global $pdo;
+
+        try {
+            $stmt = $pdo->prepare('SELECT * FROM user_post ORDER BY id DESC');
+            $stmt->execute();
+            return $stmt->fetchAll($fetchType);
+        } catch (PDOException $e) {
+            // Handle the exception, you can log it or return an error message
+            return false;
+        }
+    }
+
+    public function readPostById($id, $fetchType = PDO::FETCH_OBJ){
+        global $pdo;
+
+        try {
+            $stmt = $pdo->prepare('SELECT * FROM user_post WHERE id=?');
+            $stmt->execute([$id]);
+            return $stmt->fetch($fetchType);
+        } catch (PDOException $e) {
+            // Handle the exception, you can log it or return an error message
+            return false;
+        }
+    }
+
+    // UPDATE POST
+    public function updatePost($id, $title, $content){
+        global $pdo;
+
+        try {
+            $stmt = $pdo->prepare('UPDATE user_post SET title=?, content=? WHERE id=?');
+            $stmt->execute([$title, $content, $id]);
+            // You can return true here to indicate a successful update
+            return true;
+        } catch (PDOException $e) {
+            // Handle the exception, you can log it or return an error message
+            return false;
+        }
+    }
+
+    public function deletePost($id){
+        global $pdo;
+
+        try {
+            $stmt = $pdo->prepare("DELETE FROM user_post WHERE id=?");
+            $stmt->execute([$id]);
+            // You can return true here to indicate a successful deletion
+            return true;
+        } catch (PDOException $e) {
+            // Handle the exception, you can log it or return an error message
+            return false;
+        }
+    }
 }
-<div></div>
-}
+?>
